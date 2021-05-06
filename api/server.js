e021c57937b688c086dd6a9f56e28208189ba61c
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-06 15:59:22 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -13,9 +13,9 @@ server.use("/api/characters", CharactersRouter);
 server.use(express.static(path.join(__dirname, "client/build")));
 
 //SANITY CHECK ENDPOINT
-server.get("/", (req, res, next)=>{
-    res.json({message: "API Up"});
-});
+// server.get("/", (req, res, next)=>{
+//     res.json({message: "API Up"});
+// });
 
 server.use("*", (req, res)=>{
     res.sendfile(path.join(__dirname, "client/build", "index.html"));
