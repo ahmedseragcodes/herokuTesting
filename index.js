c46bae2fcda428e47e47f78e73c7d903de6860ab
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-06 12:37:11 -0400

    installed dependencies and setup index js

---
+++ index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const server = require("./api/server");
+const PORT = process.env.PORT || 1234
+
+server.listen(PORT, ()=>{
+    console.log(`Listening On Port ${PORT}`);
+});
