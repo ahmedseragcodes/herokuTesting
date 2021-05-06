8d20fea4c36579517843ced842f83e61f341ca6d
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-06 13:14:56 -0400

    knexfile and table and seed successfully deployed

--- data/migrations/20210506165013_Characters.js
+++ data/migrations/20210506165013_Characters.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable("Characters", (tbl)=>{
+    tbl.increments("character_id")
+    tbl.string("character_name").notNullable().unique()
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists("Characters")
 };
