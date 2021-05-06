6f4fd9aff0a62731ddb50f8e16096bb4594ece73
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-06 15:27:37 -0400

    pulling data from API for frontend now

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,26 @@ class App extends React.Component {
     super();
     this.state = {
       characters: [
-    {name: "Frodo"},
-    {name: "Gimley"},
+    {character_name: "Frodo"},
+    {character_name: "Gimley"},
       ]}
   }
+
+componentDidMount(){
+  axios.get("http://localhost:1234/api/characters")
+  .then((res)=>{
+    console.log("SUCCEEDED GETTING ALL CHARS", res.data);
+    this.setState({
+      ...this.state,
+      characters: res.data
+    })
+    console.log(this.state.characters)
+  })
+  .catch((err)=>{
+    console.log("FAILED GETTING ALL CHARACTERS", err);
+  })
+}
+
 render(){
   return (
     <div>
@@ -22,7 +38,7 @@ render(){
         this.state.characters.map((eachChar)=>{
           return (
             <div className="characterContainer">
-              <p>{eachChar.name}</p>
+              <p>{eachChar.character_name}</p>
             </div>
           )
         })
