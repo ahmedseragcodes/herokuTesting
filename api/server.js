54a0766b4c24f31c04c3430c07826ca95845a890
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-06 15:38:06 -0400

    added code to inform heroku of static assets

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const CharactersRouter = require("./characters-router");
+const path = require("path");
 
 const server = express();
 
@@ -9,10 +10,15 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 server.use("/api/characters", CharactersRouter);
+server.use(express.static(path.join(__dirname, "client/build")));
 
 //SANITY CHECK ENDPOINT
 server.get("/", (req, res, next)=>{
     res.json({message: "API Up"});
 });
+
+server.use("*", (req, res)=>{
+    res.sendfile(path.join(__dirname, "client/build", "index.html"));
+})
 
 module.exports = server;
