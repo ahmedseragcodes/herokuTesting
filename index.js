6e4fb63d4ea613014f90f0b391202797fe1271fa
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-06 18:58:15 -0400

    wip

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const path = require("path");
 const server = require("./api/server");
 const PORT = process.env.PORT || 1234
-server.use(express.static(path.join(__dirname, "client/build")));
+
+server.use(express.static(path.join(__dirname, "../client/build")));
 
 
 server.listen(PORT, ()=>{
